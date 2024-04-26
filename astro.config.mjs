39b7b520fcaa9468a3fb7cb39d36cbb9df1768bf
Daniel Costa <daniel.sousa@msn.com>
2024-04-26 11:18:49 -0300

    feat: configuração inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cardano-oeste.io",
+  integrations: [sitemap(), tailwind(), robotsTxt()],
+});
