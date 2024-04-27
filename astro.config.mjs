ac682114f3ff79782d6ffec36c9a261d0d47af07
Daniel Sousa <daniel.sousa@msn.com>
2024-04-27 10:23:36 -0300

    feat: diversas alteracoes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:4321",
+  integrations: [tailwind(), sitemap()],
+});
