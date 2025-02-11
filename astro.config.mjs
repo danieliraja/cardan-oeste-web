0ab5773a17668efe88d8606919cac346761471dd
Daniel Costa <daniel.sousa@msn.com>
2025-02-11 09:53:09 -0300

    update: atualizados a versao do astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.cardanoeste.com.br",
-  // site: "http://localhost:4321",
+  // site: "https://www.cardanoeste.com.br",
+  site: "http://localhost:4321",
   base: "/",
   integrations: [sitemap(), tailwind(), robotsTxt()],
 });
